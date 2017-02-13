13fc8c63b5ae09574cdfc5b0c94289daa4983182
shortnotion <dr.theman@gmail.com>
2017-02-13 11:45:42 +0100

    move IndexPage from components to containers folder

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 // import Home from '../components/Home';
-import IndexPage from '../osc-params/components/IndexPage';
+import IndexPage from '../osc-params/containers/IndexPage';
 
 export default class HomePage extends Component {
   render() {
--- app/osc-params/containers/IndexPage.js
+++ app/osc-params/containers/IndexPage.js
@@ -2,12 +2,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './IndexPage.css';
-import Client from './Client';
+import Client from '../components/Client';
 
 export default class IndexPage extends Component {
   render() {
     return (
       <div className={styles.container}>
+        <Link to="/counter">to Counter</Link>
         <Client/>
       </div>
     );
